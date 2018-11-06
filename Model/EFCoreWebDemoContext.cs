00c97da488c05b7a172e2aaf4db2a658ff7f95b5
Katelynn Campbell <klyncamp23@gmail.com>
2018-11-05 19:55:55 -0500

    entity framework core

---
+++ Model/EFCoreWebDemoContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreWebDemo
+{
+    public class EFCoreWebDemoContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\;Database=EFCoreWebDemo;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
